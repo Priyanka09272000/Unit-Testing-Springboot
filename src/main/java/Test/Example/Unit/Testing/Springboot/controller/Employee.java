d42577624415aa0a6ef014a2206512823e650594
Priyanka09272000 <priyanka.prajapat.sbg@gmail.com>
2023-01-13 21:42:37 +0530

    Initial version

---
+++ src/main/java/Test/Example/Unit/Testing/Springboot/controller/Employee.java
@@ -0,0 +1,10 @@
+package Test.Example.Unit.Testing.Springboot.controller;
+
+public class Employee {
+    int employee_id;
+    String name;
+    public void print() {
+        System.out.println("new code");
+        System.out.println("welcome");
+    }
+}
